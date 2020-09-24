253e3db55c44264d815ba4d1720819db8c790921
John Clyde Gonzaga <alinoclyde.juan24@gmail.com>
2020-09-24 23:04:35 +0800

    Update pyloadcentral.py

--- pyloadcentral.py
+++ pyloadcentral.py
@@ -4,17 +4,6 @@ from eload.models import products
 import urllib.parse
 import requests
 
-# {'uid': '639234332224', 'auth': '24708c5f2898ed8b75d36da4e19c753c', 'pcode': 'ZTEST1', 'to': '09060583920', 'rrn': 'CLY2647004542'}
-# https://loadcentral.net/sellapi.do?uid=639234332224&auth=24708c5f2898ed8b75d36da4e19c753c&pcode=ZTEST1&to=09060583920&rrn=CLY2647004542
-
-# <RRN>CLY2647004542</RRN><RESP>0</RESP><TID>WB2871388051</TID><BAL>12.5725</BAL><EPIN>PIN1 124601 PIN2 655466</EPIN><ERR>Success</ERR>
-
-# {'uid': '639234332224', 'auth': '24708c5f2898ed8b75d36da4e19c753c', 'rrn': 'CLY2647004542'}
-# https://loadcentral.net/sellapiinq.do?uid=639234332224&auth=24708c5f2898ed8b75d36da4e19c753c&rrn=CLY2647004542
-
-# <RRN>CLY2647004542</RRN><RESP>0</RESP><EBAL>12.5725</EBAL><TID>47666467</TID><RET>0</RET><REF>WB2871388051</REF><EPIN>PIN1 124601 PIN2 655466</EPIN><ERR>Success</ERR>
-
-
 class LoadCentral():
 	def __init__(self, uid, password, host = 'https://loadcentral.net/'):
 		self.uid = uid
@@ -56,25 +45,3 @@ class LoadCentral():
 				'rrn': rrn
 				}
 			})
-
-def update_product_list(url='http://loadcentral.com.ph/products'):
-	s = requests.Session()
-	response = s.get(url)
-	soup = BeautifulSoup(response.text, 'html.parser')
-	table = soup.find('table', id='tablepress-66')
-	data_list = table.find('tbody')
-
-	entries = []
-
-	for row in data_list.find_all('tr'):
-		p_name = row.find('td', class_='column-1').get_text()
-		p_code = row.find('td', class_='column-2').get_text()
-		p_desc = row.find('td', class_='column-3').get_text()
-		entries.append(
-			products(product_code = p_code, product_name = p_name, product_description = p_desc)
-		)
-	try:
-		products.objects.bulk_create(entries)
-		return True
-	except:
-		return False
